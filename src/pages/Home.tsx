07e6c4d0ce43b3906835c90c73eff727c488a034
Julio Rodrigues <julio.cesar@saudegarantida.com.br>
2021-06-22 14:40:15 -0300

    Autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import { useHistory } from 'react-router-dom';
 
+import { auth, firebase } from '../services/firebase';
+
+
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
+
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
@@ -12,8 +16,17 @@ export function Home(){
 
     const history = useHistory();
 
-    function navigateToNewRoom(){
-        history.push('/rooms/new')
+    function handleCreateRoom(){
+
+        const provider = new firebase.auth.GoogleAuthProvider(); 
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+            history.push('/rooms/new')
+
+        })
+
+
     }
 
     return (
@@ -28,7 +41,7 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do google" />
                         Crie sua sala com o Google
                     </button>
